3893b8b7c328c20bc2ad3f5cd6664b3a80973f91
Lim H <limdauto@gmail.com>
2016-02-20 17:08:42 +0000

    Add numericBinop

--- Eval.hs
+++ Eval.hs
@@ -11,11 +11,27 @@ eval (List (Atom func : args)) = apply func $ map eval args
 
 
 apply :: String -> [LispVal] -> LispVal
-apply = undefined
+apply func args = maybe (Bool False) ($ args) (lookup func primitives)
 
 primitives :: [(String, [LispVal] -> LispVal)]
-primitives = [("+", numericBinop (+))]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
-numericBinop = undefined
+numericBinop op args = Number $ foldl1 op $ map unpackNum args
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number x) = x
+unpackNum (String s) =
+    case parsed of
+        (x:xs) -> fst x
+        _     -> 0
+    where parsed = reads s
+unpackNum (List [n]) = unpackNum n
+unpackNum _          = 0
 
